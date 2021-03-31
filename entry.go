b1f67d7a473ff13012c6237a4d0ce1d642b86f57
Shamil Gumerov <shamil.gumerov@gmail.com>
2021-03-31 20:00:49 +0300

    feat[main]: Commands

--- entry.go
+++ entry.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"loadtester/utils"
 	"net/http"
 	"strconv"
@@ -8,6 +9,8 @@ import (
 )
 
 var keepGoing = true
+var counter = 0
+var settings = utils.GetSettings()
 
 func performRequest(url string) {
 	resp, err := http.Get(url)
@@ -37,26 +40,53 @@ func performMultiRequest(url string, count int) {
 	}
 }
 
-func getCommand() {
-	var command = utils.LD_COMMAND_STOP
-
-	if command == utils.LD_COMMAND_STOP {
-		keepGoing = false
+func getCommand() (command int, params []string, err error) {
+	//if command == utils.LD_COMMAND_STOP {
+	//	keepGoing = false
+	//}
+	counter += 1
+	if counter >= settings.Seconds{
+		return utils.LD_COMMAND_STOP, nil, nil
 	}
+	if counter != 1 {
+		return utils.LD_COMMAND_KEEP, nil, nil
+	}
+
+	return utils.LD_COMMAND_DDOS, nil, nil
 }
 
 func main() {
 	utils.WriteLog("Start.")
 
-	var settings = utils.GetSettings()
+	for  {
 
-	for i := 0; i < settings.NumberOfRoutines; i++ {
-		go performMultiRequest(settings.Url+"?a="+strconv.Itoa(i), settings.RequPerRoutine)
-	}
+		command, params, err := getCommand()
+		utils.Check(err)
 
-	for i := 0; i < settings.Seconds; i++ {
+		switch command {
+
+		case utils.LD_COMMAND_DDOS:
+			for i := 0; i < settings.NumberOfRoutines; i++ {
+				go performMultiRequest(settings.Url+"?a="+strconv.Itoa(i), settings.RequPerRoutine)
+			}
+			fmt.Println(params)
+
+		case utils.LD_COMMAND_STOP:
+			keepGoing = false
+
+		case utils.LD_COMMAND_KEEP:
+			keepGoing = true
+
+		default:
+			keepGoing = true
+		}
+
+		if !keepGoing{
+			break
+		}
+
 		time.Sleep(1000 * time.Millisecond)
-		//getCommand()
+
 	}
 
 	utils.WriteLog("Finished.")
--- utils/commands.go
+++ utils/commands.go
@@ -1,3 +1,6 @@
 package utils
 
 const LD_COMMAND_STOP = 0
+const LD_COMMAND_DDOS = 1
+const LD_COMMAND_KEEP = 2
+
